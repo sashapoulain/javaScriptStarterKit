ea3539cc5effa139db14a561bdbc086229003474
sashapoulain <62030982+sashapoulain@users.noreply.github.com>
2021-10-19 00:02:46 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,28 @@
+console.log("merhaba kodlama.io");
+
+let dolarDun = 9.20;
+let dolarBugun = 9.30;
+console.log(dolarDun);
+
+
+const euroDun = 11.2;
+//const ile sabit tanımlanır ve bir kez tanımlayabilirsin.
+
+console.log(euroDun);
+
+
+
+//birden fazla veriyi aynı yerde tutmak için
+let konutKredileri = ["konut kredileri", "emlak kredileri","kamu kredileri", "özel konut kredileri"]
+
+console.log("ul")
+for(let i = 0; i<konutKredileri.length; ++i){
+    console.log("<li>"+konutKredileri[i]+"</li>");
+
+}
+console.log("/ul")
+
+/* es7 ve material icon */
+
+
+console.log(konutKredileri)
